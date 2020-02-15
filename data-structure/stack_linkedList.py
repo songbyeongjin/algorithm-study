f729d40f4956453ea1625c670a73b96622c1750e
songbyeongjin <song4489z@naver.com>
2020-02-15 15:52:47 +0900

    stack_linkedList correct typo

--- data-structure/stack_linkedList.py
+++ data-structure/stack_linkedList.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.prev = None
 
+
 class Stack:
     def __init__(self):
         self.head = None
@@ -18,12 +19,14 @@ class Stack:
 
     def push(self, data):
         print(f"< push -> {data} >")
+        
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
         else:
             new_node.prev = self.head
             self.head = new_node
+
         print(self)
 
     def pop(self):
@@ -43,19 +46,19 @@ class Stack:
         return isempty 
 
 
-if __name__=="__main__":
-    m_stack = Stack()
-    m_stack.isEmpty()
-    m_stack.push(1)
-    m_stack.push(2)
-    m_stack.push(3)
-    m_stack.pop()
-    m_stack.isEmpty()
-    m_stack.push(4)
-    m_stack.pop()
-    m_stack.push(5)
-    m_stack.pop()
-    m_stack.pop()
-    m_stack.pop()
-    m_stack.pop()
-    m_stack.isEmpty()
+
+m_stack = Stack()
+m_stack.isEmpty()
+m_stack.push(1)
+m_stack.push(2)
+m_stack.push(3)
+m_stack.pop()
+m_stack.isEmpty()
+m_stack.push(4)
+m_stack.pop()
+m_stack.push(5)
+m_stack.pop()
+m_stack.pop()
+m_stack.pop()
+m_stack.pop()
+m_stack.isEmpty()
